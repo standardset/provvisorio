95f2a621f1db87c68a6691df8cd03a579a8b85c5
standardset <adamo.citraro@gmail.com>
2016-12-15 09:43:39 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,6 @@
 @section('benvenuti')
 <div>
 <!-- <p><a href="mailto:info@ellelocarno.ch">info@ellelocarno.ch</a></p> -->
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ea8K3F-mdUs" frameborder="0" allowfullscreen></iframe>
 </div>
 @endsection
