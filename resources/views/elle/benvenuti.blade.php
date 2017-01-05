56c742b35b9d071186ac05f4d6878247342dc354
standardset <adamo.citraro@gmail.com>
2017-01-05 09:57:07 +0100

    Update benvenuti.blade.php

--- resources/views/elle/benvenuti.blade.php
+++ resources/views/elle/benvenuti.blade.php
@@ -29,8 +29,7 @@
 </div>
 </body>
             
-    @yield('menu')
-    @yield('benvenuti')
+   @yield('benvenuti')
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
