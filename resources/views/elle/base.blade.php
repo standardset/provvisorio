2012043fa0b73862ca3d34001eec98d8626011c0
standardset <adamo.citraro@gmail.com>
2017-01-06 14:51:28 +0100

    Update base.blade.php

--- resources/views/elle/base.blade.php
+++ resources/views/elle/base.blade.php
@@ -33,7 +33,7 @@
 </div>
 </body>
             
-   @yield('home')
+   @yield('video')
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
