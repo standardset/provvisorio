f84111bc694ca0f4d7b29465cb4ef7108112600a
standardset <adamo.citraro@gmail.com>
2017-01-06 13:37:53 +0100

    Create welcome_blade.php

---
+++ resources/views/elle/welcome_blade.php
@@ -0,0 +1,10 @@
+@extends('elle.benvenuti')
+
+@section('benvenuti')
+<div>
+<p><a href="mailto:info@ellelocarno.ch">- info@ellelocarno.ch -</a></p>
+<br/>
+<br/>
+<iframe width="280" height="157" src="https://www.youtube.com/embed/3muFILs4Dw8" frameborder="0" allowfullscreen></iframe>
+</div>
+@endsection
