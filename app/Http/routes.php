1843f4d486f254fdaaac51f9cc6fa8c15fdab164
standardset <adamo.citraro@gmail.com>
2017-01-06 11:30:09 +0100

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,7 +24,7 @@ Route::group(['middleware' => ['web']], function () {
         return view('welcome');
     })->middleware('guest');
     Route::get('/spazi', function () {
-        return view('spazi.blade');
+        return view('elle.spazi');
     });
     Route::get('/tasks', 'TaskController@index');
     Route::post('/task', 'TaskController@store');
