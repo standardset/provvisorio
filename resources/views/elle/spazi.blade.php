a39f245308ac1ed0b1f08f700783ae8f320a0b92
standardset <adamo.citraro@gmail.com>
2017-01-06 15:33:16 +0100

    Update spazi.blade.php

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -1,46 +1,7 @@
+@extends('elle.base')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>ELLE LOCARNO</title>
-
-    <!-- Google font
-================================================== -->
-<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<!-- fine google font
-================================================== -->
-
-    <!-- Styles -->
-   <link href="/css/stili.css" rel="stylesheet">
-
-
-</head>
-<body class="cambia_colore" style="text-align: center;">
-<div class="menu"> 
-<ul>
-<li><a href="/spazi">spazi</a></li>
-<li><a href="/">home</a></li>
-
-</ul>
+@section('spazi')
 <div>
-<br/>
-<br/>
-<br/>
-<br/>
-<img src="/img/elle.png" alt="" />
-
+<a href="img/spazi_elle.pdf"><p>Gli spazi</p></a>
 </div>
-</body>
-            
-   @yield('home')
-
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-</body>
-</html>
+@endsection
