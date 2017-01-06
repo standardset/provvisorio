0840b54925aa88fcae38da0448ae66609a038b52
standardset <adamo.citraro@gmail.com>
2017-01-06 14:47:59 +0100

    Update spazi.blade.php

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -36,7 +36,7 @@
 </div>
 </body>
             
-   @yield('benvenuti')
+   @yield('home')
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
