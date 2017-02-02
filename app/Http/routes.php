038cdd4c4b38c7ade2db6ecd1a74c98894e9f530
standardset <adamo.citraro@gmail.com>
2017-02-02 14:26:06 +0100

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,7 @@
 */
 Route::group(['middleware' => ['web']], function () {
     Route::get('/', function () {
-        return view('elle.video');
+        return view('elle.home');
     })->middleware('guest');
     Route::get('/spazi', function () {
         return view('elle.spazi');
