aaf9007a35b50938980afa2cca733d26b29aa667
standardset <adamo.citraro@gmail.com>
2017-02-02 17:09:21 +0100

    dfggfd

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -6,13 +6,13 @@
 <p>Gli spazi</p>
 <br/><br/>
 <p>Piano interrato</p>
-<img src="img/locali_piano_interrato.svg" alt="" width="500px" height="*"/>
+<img src="img/locali_piano_interrato.svg" alt="" width="300px" height="*"/>
 <br/>
 <p>Primo piano</p>
-<img src="img/locali_primo_piano.svg" alt="" width="500px" height="*" />
+<img src="img/locali_primo_piano.svg" alt="" width="300px" height="*" />
 <br/>
 <p>Terzo piano</p>
-<img src="img/locali_terzo_piano.svg" alt=""  width="500px" height="*"/>
+<img src="img/locali_terzo_piano.svg" alt=""  width="300px" height="*"/>
 
 </div>
 @endsection
