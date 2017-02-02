b7f1490d2696d6a34c87604a945b7c61f52e381b
standardset <adamo.citraro@gmail.com>
2017-02-02 17:12:38 +0100

    fdfd

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -10,16 +10,29 @@
 <img src="img/locali_piano_interrato.svg" alt="" width="300px" height="*"/>
 <br/>
 <br/>
+<br/>
+<br/>
+<br/>
+<br/>
 
 <p>Primo piano</p>
 <br/>
 <img src="img/locali_primo_piano.svg" alt="" width="300px" height="*" />
 <br/>
 <br/>
+<br/>
+<br/>
+<br/>
+<br/>
 
 <p>Terzo piano</p>
 <br/>
 <img src="img/locali_terzo_piano.svg" alt=""  width="300px" height="*"/>
+<br/>
+<br/>
+<br/>
+<br/>
+
 
 </div>
 @endsection
