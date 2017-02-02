8b2a4f9651e09f251cdb1b3ea073ef80b8c98a8f
standardset <adamo.citraro@gmail.com>
2017-02-02 17:11:20 +0100

    dfgfd

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -3,15 +3,22 @@
 @section('spazi')
 <div>
 
-<p>Gli spazi</p>
+
 <br/><br/>
 <p>Piano interrato</p>
+<br/>
 <img src="img/locali_piano_interrato.svg" alt="" width="300px" height="*"/>
 <br/>
+<br/>
+
 <p>Primo piano</p>
+<br/>
 <img src="img/locali_primo_piano.svg" alt="" width="300px" height="*" />
 <br/>
+<br/>
+
 <p>Terzo piano</p>
+<br/>
 <img src="img/locali_terzo_piano.svg" alt=""  width="300px" height="*"/>
 
 </div>
