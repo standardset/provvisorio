2abec357c4c78a57c4443af557387454c65eb417
standardset <adamo.citraro@gmail.com>
2017-02-02 17:02:15 +0100

    piani

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -5,6 +5,14 @@
 
 <p>Gli spazi</p>
 <br/><br/>
-<a href="img/spazi_elle.pdf"><img src="img/piani.jpg" alt="" /></a>
+Piano interrato
+<img src="img/locali_piano_interrato.svg" alt="" />
+<br/>
+Primo piano
+<img src="img/locali_primo_piano.svg" alt="" />
+<br/>
+Terzo piano
+<img src="img/locali_terzo_piano.svg" alt="" />
+
 </div>
 @endsection
