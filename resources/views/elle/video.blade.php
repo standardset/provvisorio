354f4d1c3ab3e3f15b243f2431c33dfe9e39c3ad
standardset <adamo.citraro@gmail.com>
2017-02-02 14:02:52 +0100

    testo intro

--- resources/views/elle/video.blade.php
+++ resources/views/elle/video.blade.php
@@ -2,7 +2,40 @@
 
 @section('video')
 <div>
-<p><a class="link_interni" href="mailto:info@ellelocarno.ch">- info@ellelocarno.ch -</a></p>
+<p>Viviamo in un tessuto sociale dove il senso d'appartenenza territoriale viene a mancare:
+tutto diventa sempre più provvisorio, si respira un clima generale d'incertezza e
+smarrimento, vengono meno i punti di riferimento socio-culturali che permettono di
+costruire la propria identità, una personale “narrazione di vita”; è sempre più difficile
+lasciare traccia di sé oggi e per il futuro.
+Una delle qualità specifiche d'azione del Forum socio-culturale del Locarnese consiste
+nel rappresentare un punto di riferimento per la socializzazione di bambini, giovani, adulti,
+anziani, favorendo “lo star bene insieme”. Uno “star bene insieme” nell’interculturalità di
+cui noi facciamo parte e di cui vogliamo cogliere l’occasione per favorire la cultura in
+senso ampio: attività come mediazione e opportunità di dialogo e di sviluppo.
+Il Locarnese, vista la moltitudine di persone, associazioni ed enti già attivi in ambito
+culturale, dispone delle risorse e delle energie necessarie per portare avanti un progetto
+come il nostro. Un progetto che vuole approdare a una casa della cultura: un luogo fisico,
+destinato alla popolazione anche per sostenere le domande dei cittadini e dei suoi gruppi
+emergenti, creando un canale culturale tra gli interventi e gli eventi della politica e i
+bisogni dei cittadini, i quali potranno ridiventare protagonisti di progetti condivisi.
+Il Forum socio-culturale del Locarnese vuole riunire associazioni culturali e artistiche del
+Locarnese disposte a condividere un ideale di creatività e convivialità. Il Forum socioculturale
+del Locarnese vuole essere un luogo di creazione e d'incontro aperto alle
+esigenze delle diverse associazioni e ai loro bisogni concreti. I locali della ex Casa d'Italia
+– caduti in disuso dopo la loro chiusura definitiva lo scorso anno – sembrano rispondere
+all'esigenza, e all'urgenza, di un centro socio-culturale locarnese, in modo particolare
+dopo lo sgombero di Casa Rossi a favore della Casa del Cinema.
+Il Forum socio-culturale del Locarnese vuole, da una parte, soddisfare i bisogni di
+associazioni già esistenti ed emergenti: bisogni che non si riducono a una richiesta di
+spazi per le singole associazioni, ma che tendono alla condivisione e alla messa in
+comune tramite l'istituzione di spazi condivisi (atelier collettivi, sale di prova o di
+montaggio); d'altra parte, il nostro progetto mira al coinvolgimento della cittadinanza
+tramite le attività che verranno proposte (spettacoli, concerti, esposizioni, dibattiti, corsi),
+ma anche grazie alla presenza di spazi di condivisione attiva: quali ad esempio aule
+didattiche per i doposcuola e punti di ritrovo per le famiglie, il tutto a favore
+dell'integrazione sociale e dello scambio intergenerazionale.
+Questo progetto si situa a metà strada fra il centro culturale e la maison de quartier e mira
+a due obiettivi complementari: uno di carattere artistico e uno di carattere sociale.</p>
 <br/>
 <br/>
 
