daee5908653039d9147e12801ce381accadf65f9
standardset <adamo.citraro@gmail.com>
2017-02-03 14:46:48 +0100

    sddfsdfs

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -29,10 +29,12 @@
 <br/>
 <img src="img/locali_terzo_piano.svg" alt=""  width="300px" height="*" usemap="#terzo_piano"/>
 <map name="terzo_piano">
-  <area shape="rect" coords="0,90,95,300" href="" alt="Terzo piano, stanza A" title="Terzo piano, stanza A" onmouseover="writeText('Terzo piano, stanza A, metri quadri XX.XX')">
+  <area shape="rect" coords="0,90,95,300" href="" alt="Terzo piano, stanza A" title="Terzo piano, stanza A" onmouseover="writeText('Terzo piano, stanza A, metri quadri 55')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.</br>Clicca sopra la stanza per sapere la disponibilità.')">
   
 </map>
-<p id="desc">Sposta il mouse sulle stanze per avere le informazioni. Clicca sopra la stanza per sapere la disponibilità.</p>
+<p id="desc">Sposta il mouse sulle stanze per avere le informazioni. 
+<br/>
+Clicca sopra la stanza per sapere la disponibilità.</p>
 <br/>
 <br/>
 <br/>
