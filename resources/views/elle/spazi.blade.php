22ca4abf82e1803857c002ed508ec1fd288f6c61
standardset <adamo.citraro@gmail.com>
2017-02-03 15:06:26 +0100

    ghfghff

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -24,14 +24,19 @@
 <br/>
 <br/>
 <br/>
-
+<!--//////////////////TERZO PIANO/////////////////////-->
 <p><span style="background-color:#f8e0b5">&nbsp;TERZO PIANO&nbsp;</span></p>
 <br/>
 <img src="img/locali_terzo_piano.svg" alt=""  width="300px" height="*" usemap="#terzo_piano"/>
 
 <map name="terzo_piano">
-<area shape="rect" coords="0,90,95,300" href="" alt="Terzo piano, stanza A" title="Terzo piano, stanza A" onmouseover="writeText('Terzo piano, stanza A, metri quadri 55')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')">
+
+<area shape="rect" coords="100,90,95,150" href="" alt="Terzo piano, stanza A" title="Terzo piano, stanza A" onmouseover="writeText('Terzo piano, stanza A, metri quadri 55')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')">
+
+<area shape="rect" coords="0,90,95,300" href="" alt="Terzo piano, stanza B" title="Terzo piano, stanza B" onmouseover="writeText('Terzo piano, stanza B, metri quadri 40')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')">
+
 </map>
+
 <br/>
 <br/>
 <p id="desc">Sposta il mouse sulle stanze per avere le informazioni. </p>
@@ -39,7 +44,7 @@
 <br/>
 <br/>
 <br/>
-
+<!--//////////////////FINE TERZO PIANO/////////////////////-->
 
 </div>
 @endsection
