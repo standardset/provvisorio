896f3ddf198f6c72bcc70d7d1d8dd418b3e8cd75
standardset <adamo.citraro@gmail.com>
2017-02-03 15:28:54 +0100

    hgfhfg

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -32,9 +32,9 @@
 
 <map name="terzo_piano">
 
-<area shape="rect" coords="0,90,95,300" onmouseover="writeText('Terzo piano, stanza A, metri quadri 55')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')"/>
+<area shape="rect" coords="0,90,95,300" onmouseover="writeText('TTerzo piano, stanza A, metri quadri 55')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')"/>
 
-<area shape="rect" coords="95,90,95,300" onmouseover="writeText('Terzo piano, stanza B, metri quadri 40')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')"/>
+
 
 </map>
 
