d0cdec6defe4ddcb27acdf36771ad19f05d65800
standardset <adamo.citraro@gmail.com>
2017-02-04 10:42:04 +0100

    hfghgfgfh

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -36,7 +36,7 @@
 
 
 
-<area shape="rect" coords="200,160,95,120" onmouseover="writeText('Terzo piano, stanza B, metri quadri 40')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')"/>
+<area shape="rect" coords="200,350,95,120" onmouseover="writeText('Terzo piano, stanza B, metri quadri 40')" onmouseout="writeText('Sposta il mouse sulle stanze per avere le informazioni.')"/>
 
 
 
