40538157d76c9d3084a873dc7ed4bdc2d206222d
standardset <adamo.citraro@gmail.com>
2017-02-08 10:22:15 +0100

    dfggdf

--- resources/views/elle/base.blade.php
+++ resources/views/elle/base.blade.php
@@ -62,10 +62,8 @@ function writeTextTerzo(txt) {
 <script> 
 $(document).ready(function() {
 
-    // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
     })
 
 });
