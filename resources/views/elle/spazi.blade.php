2daad0462d88fd984ddd3c347770ec68884b6358
standardset <adamo.citraro@gmail.com>
2017-02-09 10:09:08 +0100

    gfdgdf

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -110,6 +110,27 @@
 <br/>
 <!--//////////////////FINE TERZO PIANO/////////////////////-->
 
+<br/>
+<br/>
+<br/>
+<br/><br/>
+<br/>
+<br/>
+<br/><br/>
+<br/>
+<br/>
+<br/><br/>
+<br/>
+<br/>
+<br/><br/>
+<br/>
+<br/>
+<br/>
+
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=adamocitraro%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+
+
 
 
 </div>
