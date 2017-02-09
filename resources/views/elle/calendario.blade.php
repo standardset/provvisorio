1dae84c11ce0423a6b9ea6e5a4800ff73231fdf1
standardset <adamo.citraro@gmail.com>
2017-02-09 15:05:00 +0100

    hfgfgh

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -3,7 +3,7 @@
 @section('calendario')
 <div>
 
-<iframe src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=ubls46tq1uo6dhu78gjf4tf3hg%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=ubls46tq1uo6dhu78gjf4tf3hg%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="600" height="800" frameborder="0" scrolling="no"></iframe>
 
 
 
