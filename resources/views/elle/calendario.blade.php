af0eb67e86d55dc73ad0787e6585648c1df43e30
standardset <adamo.citraro@gmail.com>
2017-02-09 11:40:16 +0100

    Update calendario.blade.php

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -1,1 +1,13 @@
-sdfdssdf
+@extends('elle.base')
+
+@section('calendario')
+<div>
+
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=adamocitraro%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
+
+
+
+
+
+</div>
+@endsection
