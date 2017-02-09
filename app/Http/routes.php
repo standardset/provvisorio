dc8785c46167bc6050863af0d02835b42181c4ce
standardset <adamo.citraro@gmail.com>
2017-02-09 11:43:09 +0100

    ghfgfh

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,6 +26,11 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/spazi', function () {
         return view('elle.spazi');
     });
+    
+    Route::get('/calendario', function () {
+        return view('elle.calendario');
+    });
+    
     Route::get('/tasks', 'TaskController@index');
     Route::post('/task', 'TaskController@store');
     Route::delete('/task/{task}', 'TaskController@destroy');
