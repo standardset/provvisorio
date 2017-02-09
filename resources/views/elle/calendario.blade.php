c62f617e61595632888706ee6b6b3802a8ef229b
standardset <adamo.citraro@gmail.com>
2017-02-09 11:46:07 +0100

    fdgfgd

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -3,8 +3,7 @@
 @section('calendario')
 <div>
 
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=adamocitraro%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
-
+<iframe src="https://calendar.google.com/calendar/embed?src=ellelocarnocalendario%40gmail.com&ctz=Europe/Zurich" style="border: 0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
 
 
 
