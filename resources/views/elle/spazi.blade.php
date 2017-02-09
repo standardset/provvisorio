a525f9bbb7377a3747d7e522475f72217dae4d30
standardset <adamo.citraro@gmail.com>
2017-02-09 10:31:59 +0100

    dfgfgd

--- resources/views/elle/spazi.blade.php
+++ resources/views/elle/spazi.blade.php
@@ -127,7 +127,7 @@
 <br/>
 <br/>
 
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=adamocitraro%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=adamocitraro%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
 
 
 
