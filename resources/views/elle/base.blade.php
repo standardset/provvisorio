02f7820560b50ed278bcf99469e365d899d0f04f
standardset <adamo.citraro@gmail.com>
2017-02-18 20:31:17 +0100

    ghfghfhfg

--- resources/views/elle/base.blade.php
+++ resources/views/elle/base.blade.php
@@ -58,7 +58,7 @@ function writeTextTerzo(txt) {
 
 <div class="fascia_menu">
 
-<img style="float: left; padding-left: 20px; margin-top: 10px;" src="/img/logo_elle_fascia_menu.png" alt="logo spazio elle" height="55px" width="*"/>
+<img style="float: left; padding-left: 25px; margin-top: 10px;" src="/img/logo_elle_fascia_menu.png" alt="logo spazio elle" height="55px" width="*"/>
 
 
 <div id="indirizzo">
