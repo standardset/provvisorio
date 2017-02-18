42643b595979ab5572fa3a02f518d608a49c2968
standardset <adamo.citraro@gmail.com>
2017-02-18 10:41:36 +0100

    ghfhfg

--- resources/views/elle/mappa.blade.php
+++ resources/views/elle/mappa.blade.php
@@ -6,6 +6,10 @@
 <br/>
 <br/>
 <br/>
+<br/>
+<br/>
+<br/>
+<br/>
 <div>
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d690.7968031229334!2d8.797980429228454!3d46.166928898694714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4785c9cd6bf5e069%3A0x8775dfdcdb8ff1f0!2sPiazza+G.+Pedrazzini+12%2C+6600+Locarno!5e0!3m2!1sit!2sch!4v1487410377163" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
