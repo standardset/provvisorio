79dc4f9390de5de7a8d1fc8fc5e794f9365568f9
standardset <adamo.citraro@gmail.com>
2017-02-28 13:46:38 +0100

    dsffds

--- resources/views/elle/home.blade.php
+++ resources/views/elle/home.blade.php
@@ -14,7 +14,7 @@
 <br/>
 inviateci le vostre disponibilità e le proposte di attività. Ci incontreremo quindi divisi già in gruppi di lavoro entro la fine di marzo, per discutere e cominciare a organizzare i lavori. Diffondete dunque questo annuncio e affrettatevi a compilare il formulario elettronico, ricordandovi che non c'è comunque data di scadenza e che sarà sempre possibile presentare ulteriori progetti! Chi, nel corso dell'ultima assemblea, avesse già proposto un'attività ma volesse comunque descriverla più precisamente, può farlo sempre attraverso questo link:
 <br/>
-<a href="https://goo.gl/forms/u76dNfReP1vEQpe42"> Vai al formulario elettronico</a> 
+<a class="link_interni" href="https://goo.gl/forms/u76dNfReP1vEQpe42"> Vai al formulario elettronico</a> 
 <br/>
 Per coloro che si sono resi disponibili ad aiutarci nell'allestimento degli spazi, offrendo il proprio tempo oppure del materiale, vi scriveremo una volta ricevuto il permesso di entrare nella casa da parte del Comune. Questo dovrebbe avvenire verso la fine di marzo. Per chi invece avesse urgenza di liberarsi di materiale, può scriverci sempre a  info@ellelocarno.ch e spedire alcune foto. Vi ricontatteremo al più presto e ci occuperemo noi del trasporto.
 <br/>
