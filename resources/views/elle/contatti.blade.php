2aafd34dce8cc47c44679406b0c07cfaf8462446
standardset <adamo.citraro@gmail.com>
2017-03-02 14:15:00 +0100

    dfgfgd

--- resources/views/elle/contatti.blade.php
+++ resources/views/elle/contatti.blade.php
@@ -1,1 +1,77 @@
+@extends('elle.base')
+
+@section('contatti')
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<div>
+
+<h1 class="titolo_pagina">
+Le persone responsabili 
+<br/>dei settori dello Spazio ELLE
+</h1>
+
+<p>
+<br/>
+<br/> 
+Musica
+<br/>
+Ronnie Rodriguez
+<br/>
+musica@ellelocarno.ch
+<br/>
+<br/>
+Arti performative
+<br/>
+Marco Cupellari
+<br/>
+artiperformative@ellelocarno.ch
+<br/>
+<br/>
+Arti visive
+<br/>
+Riccardo Lisi
+<br/>
+artivisive@ellelocarno.ch
+<br/>
+<br/>
+Socio-culturale
+<br/>
+Saffia Shaukat
+<br/> 
+socioculturale@ellelocarno.ch
+<br/>
+<br/>
+Letteratura e cinema
+<br/>
+Yari Moro
+<br/> 
+letteraturacinema@ellelocarno.ch
+<br/>
+<br/>
+Atelier
+<br/>
+Matteo Minetti
+<br/> 
+atelier@ellelocarno.ch
+<br/>
+<br/>
+Animazione
+<br/>
+Lorenzo Perucchi
+<br/>
+animazione@ellelocarno.ch
+<br/>
+<br/>
+
+</p>
+
+</div>
+@endsection
 
