950569e0db6ceb5f71c1256c6d16d9342eaaa405
standardset <adamo.citraro@gmail.com>
2017-06-27 21:08:48 +0200

    fgfgdgdf

--- resources/views/elle/home.blade.php
+++ resources/views/elle/home.blade.php
@@ -10,15 +10,8 @@
  
  
  
-<p class="testo_giustificato">Cari amici e futuri partners e utenti del quasi nato Spazio ELLE,
-inviateci le vostre disponibilità e le proposte di attività. Ci incontreremo quindi divisi già in gruppi di lavoro entro la fine di marzo, per discutere e cominciare a organizzare i lavori. Diffondete dunque questo annuncio e affrettatevi a compilare il <a class="collegamenti_interni" href="https://goo.gl/forms/u76dNfReP1vEQpe42"> formulario elettronico</a> , ricordandovi che non c'è comunque data di scadenza e che sarà sempre possibile presentare ulteriori progetti! Chi, nel corso dell'ultima assemblea, avesse già proposto un'attività ma volesse comunque descriverla più precisamente, può farlo sempre attraverso il
-<a class="collegamenti_interni" href="https://goo.gl/forms/u76dNfReP1vEQpe42"> formulario elettronico</a> 
-<br/>
-Per coloro che si sono resi disponibili ad aiutarci nell'allestimento degli spazi, offrendo il proprio tempo oppure del materiale, vi scriveremo una volta ricevuto il permesso di entrare nella casa da parte del Comune. Questo dovrebbe avvenire verso la fine di marzo. Per chi invece avesse urgenza di liberarsi di materiale, può scriverci sempre a <a class="collegamenti_interni" href="mailto:info@ellelocarno.ch">info@ellelocarno.ch</a> e spedire alcune foto. Vi ricontatteremo al più presto e ci occuperemo noi del trasporto.
-<br/>
-<br/>
-<span style="float: right;">Gruppo di lavoro - Spazio ELLE</span></p>
-<br/>
+<p class="testo_giustificato">
+    </p>
 <br/>
 
 </div>
