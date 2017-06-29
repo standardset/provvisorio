ea84252c707b9b9a5f3f4cccf8fbbfac549fda77
standardset <adamo.citraro@gmail.com>
2017-06-29 21:10:52 +0200

    Create prova_database.php

---
+++ resources/views/elle/prova_database.php
@@ -0,0 +1,26 @@
+@extends('elle.base')
+
+@section('contatti')
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<div>
+
+<h1 class="titolo_pagina">
+I responsabili per settore
+</h1>
+
+<p>
+
+hello <?php echo="world"; ?>
+</p>
+
+</div>
+@endsection
+
