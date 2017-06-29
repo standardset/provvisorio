c704fc7c9a60b2a84d44a9fb2622ac54bfcb4018
standardset <adamo.citraro@gmail.com>
2017-06-29 21:12:35 +0200

    Create prova_database.blade.php

---
+++ resources/views/elle/prova_database.blade.php
@@ -0,0 +1,25 @@
+@extends('elle.base')
+
+@section('prova_database')
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<div>
+
+<h1 class="titolo_pagina">
+I responsabili per settore
+</h1>
+
+<p>
+Ciao <?php echo="mondo";
+</p>
+
+</div>
+@endsection
+
