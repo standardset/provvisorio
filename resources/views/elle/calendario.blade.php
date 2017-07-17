1905394fdee1b1b0464cc0bbef0819d9ca633be8
standardset <adamo.citraro@gmail.com>
2017-07-17 17:55:07 +0200

    fgghf

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -5,19 +5,17 @@
 
 
 
-<?php echo '<div class="evento">
+<?php    
+    
+for( $contatore=1; $contatore<10; $contatore++ ){
+		echo '<div class="evento">
 <img class="ridimensiona" src="/img/placeholder.jpg" width="150px" height="auto"/> 
 
 <h2 class="titolo_evento">
        Questo è il titolo <br/>dell evento numero uno
 </h2>
    
-</div>'; 
-    
-    
-    
-for( $contatore=1; $contatore<10; $contatore++ ){
-		echo "siamo entrati nel ciclo per la " . $contatore . " volta <br>";
+</div>';
 	}
 
 
