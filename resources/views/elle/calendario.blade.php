5a9d9d8cd5591d03f570f58d8e99b0f23cdbf1af
standardset <adamo.citraro@gmail.com>
2017-07-17 16:52:02 +0200

    ghfhgfgfh

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -4,9 +4,13 @@
 <div class="flexbox">
 
 
-<div id="uno"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
-<div id="due"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero due</div>
-<div id="tre"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero tre</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+
 
 
 
