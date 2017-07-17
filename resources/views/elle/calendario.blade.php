8d5f9095c9560961e72cea6d52ba138295f22ac5
standardset <adamo.citraro@gmail.com>
2017-07-17 16:41:50 +0200

    gfhghf

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -4,9 +4,9 @@
 <div class="flexbox">
 
 
-<div id="uno">Questo è il titolo dell'evento numero uno</div>
-<div id="due">Questo è il titolo dell'evento numero due</div>
-<div id="tre">Questo è il titolo dell'evento numero tre</div>
+<div id="uno"><img src="/img/placeholder.jpg"/> <br/>Questo è il titolo dell'evento numero uno</div>
+<div id="due"><img src="/img/placeholder.jpg"/> <br/>Questo è il titolo dell'evento numero due</div>
+<div id="tre"><img src="/img/placeholder.jpg"/> <br/>Questo è il titolo dell'evento numero tre</div>
 
 
 
