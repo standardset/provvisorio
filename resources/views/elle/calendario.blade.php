c77ba8fc8590943c5584c24a9b010c8687b64bd0
standardset <adamo.citraro@gmail.com>
2017-07-17 13:28:00 +0200

    gfggfd

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -3,7 +3,10 @@
 @section('calendario')
 <div>
 
-<iframe src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=ubls46tq1uo6dhu78gjf4tf3hg%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=Europe%2FZurich" style="border-width:0" width="600" height="800" frameborder="0" scrolling="no"></iframe>
+
+<div id="1">1</div>
+<div id="2">2</div>
+<div id="3">3</div>
 
 
 
