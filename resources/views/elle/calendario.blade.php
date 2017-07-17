2dc2da5ce7083fcccb79c7e2719278f12271d0a9
standardset <adamo.citraro@gmail.com>
2017-07-17 16:45:48 +0200

    dffd

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -4,9 +4,9 @@
 <div class="flexbox">
 
 
-<div id="uno"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo dell'evento numero uno</div>
-<div id="due"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo dell'evento numero due</div>
-<div id="tre"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo dell'evento numero tre</div>
+<div id="uno"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div id="due"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero due</div>
+<div id="tre"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero tre</div>
 
 
 
