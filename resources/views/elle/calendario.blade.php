a9ec1d0dd19f821d6613c93d8739c0f69d9b20d4
standardset <adamo.citraro@gmail.com>
2017-07-17 17:03:18 +0200

    dggfd

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -11,6 +11,13 @@
 <div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
 <div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
 
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+<div class="evento"><img src="/img/placeholder.jpg" width="150px" height="auto"/> <br/>Questo è il titolo <br/>dell'evento numero uno</div>
+
 
 
 
