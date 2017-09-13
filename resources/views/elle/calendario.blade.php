dab165da4cc0cf593922c09b573e05bcd8575e1a
standardset <adamo.citraro@gmail.com>
2017-09-13 15:48:41 +0200

    fghghfhgf

--- resources/views/elle/calendario.blade.php
+++ resources/views/elle/calendario.blade.php
@@ -14,6 +14,9 @@ for( $contatore=1; $contatore<20; $contatore++ ){
 <h2 class="titolo_evento">
        Questo è il titolo <br/>dell evento numero uno
 </h2>
+<p class="testo_evento">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien.
+
+</p>
    
 </div>';
 	}
