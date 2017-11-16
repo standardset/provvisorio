8cb0b8483ca91a3be469e80409c01b08c69a3f2b
standardset <adamo.citraro@gmail.com>
2017-11-16 15:49:07 +0100

    gfhfgh

--- resources/views/elle/corsi.blade.php
+++ resources/views/elle/corsi.blade.php
@@ -8,24 +8,21 @@
 
 
 <!-- ////////////CORSO//////////////////////-->
-<div class="evento">
+<div class="corso">
 <div style="padding: 15px;">
-<h2 class="titolo_evento">3-10-17-24 NOVEMBRE <br/> LA ELLE CUISINE</h2>
+<h2 class="titolo_evento">LUNEDÌ 
+ <br/> CAPOEIRA</h2>
  
 <p class="testo_evento">
-     Pasti genuini preparati con cura utilizzando verdure dell'orto, piante selvatiche, prodotti locali, biologici, fair trade e con il minimo di imballaggi.
+     Le prime tre lezioni sono gratuite e di prova. Sconti per famiglie e studenti. Ogni lunedì: Corso adulti  Ore: 19.00-21h00 / Corso bambini (min. 5 anni).
 <br/>
 <br/>
-Tutti i venerdì 3-10-17-24 Novembre
+Ore: 18.00-19.00 
 <br/>
-Ore 12.00-13.00, 
-<br/>
-1° piano, Sala incontro
+1° piano, Sala piccola
 <br/>
 <br/>
-Info:  pranzietici@gmail.com, facebook.com/pranzietici, 
-<br/>
-Anna tel. 079 392 12 93
+Info: Matteo Beltrami, +4176 695 80 10 www.capoeiraticino.ch
 </p>
   
  </div>
@@ -33,6 +30,182 @@ Anna tel. 079 392 12 93
 </div>
 
 <!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+<!-- ////////////CORSO//////////////////////-->
+<div class="corso">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">MARTEDÌ 
+ <br/>TANGO ARGENTINO</h2>
+ 
+<p class="testo_evento">
+Le prime tre lezioni sono gratuite e per chi si iscrive in coppia  ci sarà un incentivo di -20% sul prezzo dell’abbonamento. 
+Ogni martedì: Corso principianti Ore: 19:30  / Corso intermedi-avanzati Ore: 20.30 - 1° piano, Sala piccola. 
+<br/>
+<br/>
+Info: +4178 623 37 32  - tzuppus@bluewin.ch - www.libertango.ch</p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+<!-- ////////////CORSO//////////////////////-->
+<div class="corso">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">MERCOLEDÌ
+ <br/>CORSO DI DANZA MODERNA PER ADULTI </h2>
+ 
+<p class="testo_evento">
+Corso di Danza moderna per adulti con Silvia Sabatino di Espressione Danza. Alleniamo il corpo e la mente con degli esercizi specifici ed impariamo delle divertenti coreografie. Aperto a tutti anche senza nessuna esperienza di danza. Mercoledì 4 ottobre lezione di prova. Ogni mercoledì - Ore: 18.30-19.30 - 1° piano, Sala piccola.  
+<br/>
+<br/>
+Info: Silvia Sabatino, Tel. 078 6118701</p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+<!-- ////////////CORSO//////////////////////-->
+<div class="corso">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">MERCOLEDÌ
+ <br/>CORSO LE BASI DEL DISEGNO</h2>
+ 
+<p class="testo_evento">
+ Il corso introduttivo, offre un’ampia panoramica di temi e tecniche. Dal disegno intuitivo, passando per la linea e i volumi, fino ad arrivare agli esercizi più complessi. Il tutto arricchito da consigli professionali su come migliorare costantemente, come impostare il proprio lavoro, i trucchi per ottenere di più dalle proprie capacità, cenni storici su questa nobile arte, analisi di autori e molto altro! Il modulo è un’ottima base sia per principianti che per avanzati sulla quale costruire il proprio percorso di formazione.
+Ogni mercoledì  - Ore: 20.00 - 21.30 - 1° piano, Sala piccola  
+<br/>
+<br/>
+Info: Andrea + Andrea, scuoladidisegno@gmail.com </p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+
+
+<!-- ////////////CORSO//////////////////////-->
+<div class="corso">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">GIOVEDÌ
+ <br/>HATA YOGA</h2>
+ 
+<p class="testo_evento">
+ Una lezione di Hatha Yoga aumenta la forza e la flessibilità fisica, migliora la respirazione, la concentrazione e la gestione dello stress. Scopo della pratica è promuovere il benessere e l'equilibrio del corpo e delle mente.
+Le lezioni sono composte da una prima fase di Pranayama (controllo del respiro) e riscaldamento, una parte centrale di Asana (posture tenute a lungo), seguite da una fase finale di rilassamento (Savasana).
+Il corso vuole inoltre offrire un momento di scambio e condivisione. A seguire un film di CINELLE nella stessa sala… quindi non sudate troppo! 
+Ogni giovedì - Ore: 19.00-20.15 -  1° piano, Sala piccola   
+<br/>
+<br/>
+Info: Costo 15 fr./1h15 di lezione. Corso condotto da Katia Gandolfi. katiagandolfi@hotmail.com, Tel. 078 600 40 52 </p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+
+<!-- ////////////CORSO//////////////////////-->
+<div class="corso">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">GIOVEDÌ
+ <br/>T'AI CHI CH'UAN, STILE YANG</h2>
+ 
+<p class="testo_evento">
+Armonia, quiete e movimento. Corso condotto da Jean-Martin Roy, allievo dei Maestri Petra e Toyo Kobayashi. Lezione di prova il 12 ottobre. 
+Ogni giovedì - Ore: 10.00-11.30 - 1° piano, Sala grande    
+<br/>
+<br/>
+Info: jeanmartin.roy@supsi.ch</p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+<!-- ////////////CORSO//////////////////////-->
+<div class="corso">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">GIOVEDÌ
+ <br/>BALFOLK</h2>
+ 
+<p class="testo_evento">
+Appuntamenti dedicati al Balfolk francese e alle danze popolari (collettive e di coppia) di vari paesi del mondo. Le lezioni permettono di migliorare il senso del ritmo, la coordinazione e la consapevolezza del proprio corpo nello spazio; di mettersi in gioco, acquisire autostima e "viaggiare" scoprendo i diversi paesi e le loro culture attraverso la danza. Gli incontri sono rivolti a tutti, compresi coloro che non hanno mai ballato e si sentono "negati".  Non è indispensabile essere in coppia.
+Ogni giovedì - Corso base Ore:19:00 - 20:30 / Corso avanzato
+Ore: 20:45 - 22.15 - 1° piano, Sala grande     
+<br/>
+<br/>
+Info: +41076 466 19 88 -  balfolkticino@gmail.com</p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+
+<!-- ////////////CORSO//////////////////////-->
+<div class="corso">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">CORSI INTENSIVI
+ <br/>CORSO INTENSIVO DI FLAMENCO</h2>
+ 
+<p class="testo_evento">
+Corso intensivo di Flamenco diretto da José de los Reyes Fernández "El Rin", ballerino e amico di Camarón de la Isla, Paco de Lucia e altri grandi musicisti della storia Spagnola. Una opportunità unica per incontrare un ballo ancestrale che ha più di 4000 anni. Il corso partirà solo con almeno 20 inscritti. 
+Da lunedì 30 ottobre a domenica 5 novembre - Ore 16.00-20.00 - 1° piano, Sala grande      
+<br/>
+<br/>
+Info: Gabriela Vortmann, g_vortmann@icloud.com</p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE CORSO//////////////////////-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
