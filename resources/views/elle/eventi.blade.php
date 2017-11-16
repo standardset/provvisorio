078b69bb99dd1f69567f7c5d3c18634fdb5aea35
standardset <adamo.citraro@gmail.com>
2017-11-16 14:45:52 +0100

    fghhgf

--- resources/views/elle/eventi.blade.php
+++ resources/views/elle/eventi.blade.php
@@ -54,5 +54,36 @@ Anna tel. 079 392 12 93
 <!-- ////////////FINE EVENTO//////////////////////-->
 
 
+
+<!-- ////////////EVENTO//////////////////////-->
+<div class="evento">
+<div style="padding: 15px;">
+<h2 class="titolo_evento">8 e 22 NOVEMBRE<br/> LE CinciarELLE</h2>
+ 
+<p class="testo_evento">
+     Le CinciarELLE  hanno il piacere di accogliere mamme, bambini e tutti coloro che hanno voglia di trascorrere un po' di tempo in buona compagnia, in uno spazio adatto alle chiacchiere, al gioco e all'incontro. Sarà in funzione la mescita con spuntini e merende.
+<br/>
+<br/>
+Mercoledì 8 e 22 Novembre
+<br/>
+Ore: 15.30-18.30 
+<br/>
+1° piano, Sala piccola
+<br/>
+<br/>
+Info:  Info: Diana Rodriguez 079 808.40.66
+</p>
+  
+ </div>
+   
+</div>
+
+<!-- ////////////FINE EVENTO//////////////////////-->
+
+
+
+
+
+
 </div>
 @endsection
