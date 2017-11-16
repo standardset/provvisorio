9f1f619ac86b1da1574050472067c25ae722bf8a
standardset <adamo.citraro@gmail.com>
2017-11-16 13:33:25 +0100

    hfgfghfgh

--- resources/views/elle/home.blade.php
+++ resources/views/elle/home.blade.php
@@ -7,37 +7,30 @@
 <br/>
 <br/>
 <!--<img src="/img/elle.png" alt="logo spazio elle" />-->
-
- <div style="width:400px; text-align:center; top: 200px;">
- <br/>
 <br/>
 <br/>
 <br/>
 <br/>
 <br/>
 <br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<h2 class="titolo_evento">3-10-17-24 NOVEMBRE - LA ELLE CUISINE</h2> 
+ 
+ <h2 class="titolo_evento">3-10-17-24 NOVEMBRE - LA ELLE CUISINE</h2> 
  <p>
      Pasti genuini preparati con cura utilizzando verdure dell'orto, piante selvatiche, prodotti locali, biologici, fair trade e con il minimo di imballaggi.
-     <br/>
+     <br/><br/>
 Tutti i venerdì 3-10-17-24 Novembre: Ore12.00-13.00, 1° piano, Sala incontro
-<br/>
+<br/><br/>
 Info:  pranzietici@gmail.com, facebook.com/pranzietici, Anna tel. 079 392 12 93
+</p>
 
- </p>
+<hr>
  
 </div> 
  
- 
-<p class="testo_giustificato">
-   
-   </p>
-<br/>
 
-</div>
+ <!-- <iframe src="http://us16.campaign-archive.com/?e=[UNIQID]&u=985f13037f5bcbd25e58f3ce1&id=e237d3c1fe" width="100%" height="6100" style="margin-top:-100px"></iframe>-->
+ 
+
 @endsection
+
+
