3b30108e6ccbcf3c3e622f65a7dcecf25b2a872c
standardset <adamo.citraro@gmail.com>
2017-11-16 13:24:20 +0100

    fhgfgh

--- resources/views/elle/home.blade.php
+++ resources/views/elle/home.blade.php
@@ -13,7 +13,13 @@
 <br/>
 <br/>
 <br/>
- 
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+ <div style="width=400px; text-align=center">
  <h2>3-10-17-24 NOVEMBRE - LA ELLE CUISINE</h2> 
  <p>
      Pasti genuini preparati con cura utilizzando verdure dell'orto, piante selvatiche, prodotti locali, biologici, fair trade e con il minimo di imballaggi.
@@ -24,8 +30,7 @@ Info:  pranzietici@gmail.com, facebook.com/pranzietici, Anna tel. 079 392 12 93
 
  </p>
  
- 
- 
+</div> 
  
  
 <p class="testo_giustificato">
