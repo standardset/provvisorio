7a128c505f8ad84376a5699fae543d7b072268fc
standardset <adamo.citraro@gmail.com>
2017-11-20 20:13:31 +0100

    sdsdf

--- app/Task.php
+++ app/Task.php
@@ -24,14 +24,8 @@ class Task extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return false;
     }
     
-    
-    public function isComplete() 
-        
-    {
-        
-        return false; 
-    }
+
 }
