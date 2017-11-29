9694f88c5a52b36bd4f9e1ef4d6ecfa395cb65c0
standardset <adamo.citraro@gmail.com>
2017-11-29 21:48:46 +0100

    Aggiunto foto

--- resources/views/elle/home.blade.php
+++ resources/views/elle/home.blade.php
@@ -6,7 +6,7 @@
 <br/>
 <br/>
 <br/>
-<!--<img src="/img/elle.png" alt="logo spazio elle" />-->
+
  <div style="width:450px;">
  
  <br/>
@@ -20,146 +20,7 @@
 <br/>
 <br/>
 
-<!-- ////////////EVENTO//////////////////////-->
-
- <h2 class="titolo_evento">3-10-17-24 NOVEMBRE - LA ELLE CUISINE</h2> 
- <p>
-     Pasti genuini preparati con cura utilizzando verdure dell'orto, piante selvatiche, prodotti locali, biologici, fair trade e con il minimo di imballaggi.
-     <br/><br/>
-Tutti i venerdì 3-10-17-24 Novembre: Ore 12.00-13.00, 
-<br/>
-1° piano, Sala incontro
-<br/><br/>
-Info:  pranzietici@gmail.com, facebook.com/pranzietici, 
-<br/>
-Anna tel. 079 392 12 93
-</p>
-<br/>
-<br/>
-<br/>
-<hr class="filetto_eventi">
-<br/>
-<br/>
-<br/>
-
-<!-- ////////////FINE EVENTO//////////////////////-->
- 
- <!-- ////////////EVENTO//////////////////////-->
-
- <h2 class="titolo_evento">8 e 22 NOVEMBRE -LE CinciarELLE</h2> 
- <p>
-     Le CinciarELLE  hanno il piacere di accogliere mamme, bambini e tutti coloro che hanno voglia di trascorrere un po' di tempo in buona compagnia, in uno spazio adatto alle chiacchiere, al gioco e all'incontro. Sarà in funzione la mescita con spuntini e merende. 
-     <br/><br/>
-Mercoledì 8 e 22 Novembre - Ore: 15.30-18.30, 1° piano,  
-<br/>
-Sala piccola
-<br/><br/>
-Info:  Info: Diana Rodriguez 079 808.40.66
-</p>
-<br/>
-<br/>
-<br/>
-<hr class="filetto_eventi">
-<br/>
-<br/>
-<br/>
-
-<!-- ////////////FINE EVENTO//////////////////////-->
-
-<!-- ////////////EVENTO//////////////////////-->
-
- <h2 class="titolo_evento">10-17-24 NOVEMBRE - DANZA POPOLARE "OVER 60"</h2> 
- <p>
-     Pro Senectute Creativ Center propone balli di gruppo alla scoperta delle tradizioni e musiche alla scoperta di diversi paesi. Ideale per mantenere agilità e per divertirsi insieme. Per donne e uomini. Due lezioni di prova. 
-     <br/><br/>
-Venerdì 10-17-24 Novembre: Ore 9.30 - 11.00, 1° piano, 
-<br/>
-Sala grande
-<br/><br/>
-
-Info: Patricia Martin tel. 079 264 33 31
-</p>
-<br/>
-<br/>
-<br/>
-<hr class="filetto_eventi">
-<br/>
-<br/>
-<br/>
-
-<!-- ////////////FINE EVENTO//////////////////////-->
-
-
-
-<!-- ////////////EVENTO//////////////////////-->
-
- <h2 class="titolo_evento">14 e 21 NOVEMBRE - GENITORI INSIEME</h2> 
- <p>
-     Genitorinsieme per mamme e papà di origine straniera comprende una serie di incontri in cui si condividono  le proprie esperienze di genitori e si riflette sull’educazione dei figli. Un modo per conoscere altre persone, esercitarsi nella lingua italiana, chiacchierare magari attorno ad un tavolo sorseggiando un buon té o caffé assieme alle conduttrici. 
-     <br/><br/>
-Martedì 14 e 21 Novembre: Ore 13.30 - 15.00, 1° piano,  
-<br/>
-sala incontro e piccola
-<br/><br/>
-Info: Francesca Machado 091 751 94 52 www.genitorinsieme.ch
-</p>
-<br/>
-<br/>
-<br/>
-<hr class="filetto_eventi">
-<br/>
-<br/>
-<br/>
-
-<!-- ////////////FINE EVENTO//////////////////////-->
-
-
-
-<!-- ////////////EVENTO//////////////////////-->
-
- <h2 class="titolo_evento">17 NOVEMBRE - EL FreeDay DJ SET MUSIC IMPROVISATION CON APERO</h2> 
- <p>
-     Uno fra gli appuntamenti più famosi del venerdì sera a ELLE. In funzione ci sarà una mescita con aperitivo e stuzzichini per tutti i gusti. Special Guest Dj Dingo
-     <br/><br/>
-Venerdì 17 Novembre, Ore: 18.00-22.00, 1° piano,  
-<br/>
-Sala incontro
-<br/><br/>
-Info: Lorenzo Perucchi, Tel. 079 929 98 05
-</p>
-<br/>
-<br/>
-<br/>
-<hr class="filetto_eventi">
-<br/>
-<br/>
-<br/>
-
-<!-- ////////////FINE EVENTO//////////////////////-->
-
-
-
-
-<!-- ////////////EVENTO//////////////////////-->
-
- <h2 class="titolo_evento">18 NOVEMBRE - 10 DICEMBRE ARTE CONTESTUALE - Quadri di Andrea Fiori </h2> 
- <p>
-    Esposizione di pittura a tematica sociale. L'artista è nato a Bellinzona nel 1981.<br/><br/>
-Sabato 18 Novembre, Ore: 18.00, 1° piano,  
-<br/>
-Sala piccola
-<br/><br/>
-Info: Riccardo Lisi
-</p>
-<br/>
-<br/>
-<br/>
-<hr class="filetto_eventi">
-<br/>
-<br/>
-<br/>
-
-<!-- ////////////FINE EVENTO//////////////////////-->
+<img src="/img/spazio_elle_facciata_nord.jpg" alt="logo spazio elle" />
  
 </div>
 
