1895aaca3af1e9b1d43a8d05bbce7533bcaf2e0b
standardset <adamo.citraro@gmail.com>
2017-12-13 14:41:22 +0100

    dsfdfs

--- resources/views/elle/tdl.blade.php
+++ resources/views/elle/tdl.blade.php
@@ -19,7 +19,7 @@
 
 <h3 class="data_evento"><?php echo $evento->data_inizio->format('G:i l j F Y');?></h3>
 <hr class="gradient">
-
+<br/>
 <h2 class="titolo_evento">
 <?php echo $evento->titolo; ?>
 </h2>
